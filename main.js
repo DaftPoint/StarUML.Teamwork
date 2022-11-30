1e709cc162a12a32688adaa967e460de5e01aa51
danysmall <dany.small.work@gmail.com>
2022-11-30 22:09:30 +0300

    Test if extension is working without another core code

--- main.js
+++ main.js
@@ -14,89 +14,99 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  */
-define(function (require, exports, module) {
-	"use strict";
+// define(function (require, exports, module) {
+// 	"use strict";
 
-	//# Import libs
-	var AppInit					= app.getModule("utils/AppInit");
-    var CommandManager      	= app.getModule("command/CommandManager");
-    var MenuManager        		= app.getModule("menu/MenuManager");
-    var DefaultMenus            = app.getModule("menu/DefaultMenus");
-    var ContextMenuManager      = app.getModule("menu/ContextMenuManager");
-    var ExtensionUtils          = app.getModule("utils/ExtensionUtils");
-    var Repository              = app.getModule("core/Repository");
+// 	//# Import libs
+// 	var AppInit					= app.getModule("utils/AppInit");
+//     var CommandManager      	= app.getModule("command/CommandManager");
+//     var MenuManager        		= app.getModule("menu/MenuManager");
+//     var DefaultMenus            = app.getModule("menu/DefaultMenus");
+//     var ContextMenuManager      = app.getModule("menu/ContextMenuManager");
+//     var ExtensionUtils          = app.getModule("utils/ExtensionUtils");
+//     var Repository              = app.getModule("core/Repository");
 
-    //Imported files
-    var TeamworkCommandHandler 	= require("./commandHandler/CommandHandler");
-    var LockingAttributesElement= require("./locking/LockingAttributesElement");
-    var Trigger                 = require("./trigger/Trigger");
-    var TeamworkView            = require("./teamworkView/TeamworkView");
+//     //Imported files
+//     var TeamworkCommandHandler 	= require("./commandHandler/CommandHandler");
+//     var LockingAttributesElement= require("./locking/LockingAttributesElement");
+//     var Trigger                 = require("./trigger/Trigger");
+//     var TeamworkView            = require("./teamworkView/TeamworkView");
 
-    //Overwrites
-    Repository.doOperation  = Trigger.doOperation;
+//     //Overwrites
+//     Repository.doOperation  = Trigger.doOperation;
 
-    //# Define Commands
-    var CMD_TEAMWORK         	= 'teamwork';
-    var CMD_TEAMWORK_COMMIT  	= 'teamwork.commit';
-    var CMD_TEAMWORK_OPEN       = 'teamwork.open';
-    var CMD_TEAMWORK_UPDATE  	= 'teamwork.update';
-    var CMD_TEAMWORK_CREATE  	= 'teamwork.create';
-    var CMD_TEAMWORK_LOCK  	    = 'teamwork.lock';
-    var CMD_UPDATE_LOCKS  	    = 'teamwork.lock.update';
-    var CMD_TEAMWORK_CONFIGURE  = 'teamwork.configure';
+//     //# Define Commands
+//     var CMD_TEAMWORK         	= 'teamwork';
+//     var CMD_TEAMWORK_COMMIT  	= 'teamwork.commit';
+//     var CMD_TEAMWORK_OPEN       = 'teamwork.open';
+//     var CMD_TEAMWORK_UPDATE  	= 'teamwork.update';
+//     var CMD_TEAMWORK_CREATE  	= 'teamwork.create';
+//     var CMD_TEAMWORK_LOCK  	    = 'teamwork.lock';
+//     var CMD_UPDATE_LOCKS  	    = 'teamwork.lock.update';
+//     var CMD_TEAMWORK_CONFIGURE  = 'teamwork.configure';
 
-    var CMD_LOCK_ELEMENT        = 'teamwork.element.lock';
-    var CMD_UNLOCK_ELEMENT      = 'teamwork.element.unlock';
+//     var CMD_LOCK_ELEMENT        = 'teamwork.element.lock';
+//     var CMD_UNLOCK_ELEMENT      = 'teamwork.element.unlock';
 
-    //# register commands
-    CommandManager.register("Teamwork",             		CMD_TEAMWORK,           CommandManager.doNothing);
-    CommandManager.register("Open Teamwork-Project...",     CMD_TEAMWORK_OPEN,      TeamworkCommandHandler.openProject);
-    CommandManager.register("Create Teamwork-Project", 	    CMD_TEAMWORK_CREATE,  	TeamworkCommandHandler.createProject);
-    CommandManager.register("Lock Teamwork-Project", 	    CMD_TEAMWORK_LOCK,  	TeamworkCommandHandler.lockWholeProject);
-    CommandManager.register("Update Lock-Info", 	        CMD_UPDATE_LOCKS,  	    TeamworkCommandHandler.updateProjectLockInfo);
-    CommandManager.register("Commit changes",  			    CMD_TEAMWORK_COMMIT,   	TeamworkCommandHandler.commitChanges);
-    CommandManager.register("Update Project",     		    CMD_TEAMWORK_UPDATE, 	TeamworkCommandHandler.updateProject);
-    CommandManager.register("Configure Teamwork-Server",    CMD_TEAMWORK_CONFIGURE, TeamworkCommandHandler.handleConfigure);
+//     //# register commands
+//     CommandManager.register("Teamwork",             		CMD_TEAMWORK,           CommandManager.doNothing);
+//     CommandManager.register("Open Teamwork-Project...",     CMD_TEAMWORK_OPEN,      TeamworkCommandHandler.openProject);
+//     CommandManager.register("Create Teamwork-Project", 	    CMD_TEAMWORK_CREATE,  	TeamworkCommandHandler.createProject);
+//     CommandManager.register("Lock Teamwork-Project", 	    CMD_TEAMWORK_LOCK,  	TeamworkCommandHandler.lockWholeProject);
+//     CommandManager.register("Update Lock-Info", 	        CMD_UPDATE_LOCKS,  	    TeamworkCommandHandler.updateProjectLockInfo);
+//     CommandManager.register("Commit changes",  			    CMD_TEAMWORK_COMMIT,   	TeamworkCommandHandler.commitChanges);
+//     CommandManager.register("Update Project",     		    CMD_TEAMWORK_UPDATE, 	TeamworkCommandHandler.updateProject);
+//     CommandManager.register("Configure Teamwork-Server",    CMD_TEAMWORK_CONFIGURE, TeamworkCommandHandler.handleConfigure);
 
-    CommandManager.register("Lock Element",                 CMD_LOCK_ELEMENT,       TeamworkCommandHandler.lockElement);
-    CommandManager.register("Unlock Element",               CMD_UNLOCK_ELEMENT,     TeamworkCommandHandler.unlockElement);
+//     CommandManager.register("Lock Element",                 CMD_LOCK_ELEMENT,       TeamworkCommandHandler.lockElement);
+//     CommandManager.register("Unlock Element",               CMD_UNLOCK_ELEMENT,     TeamworkCommandHandler.unlockElement);
 
-    var menu;
-    menu = MenuManager.addMenu(CMD_TEAMWORK);
-    menu.addMenuItem(CMD_TEAMWORK_OPEN);
-    menu.addMenuItem(CMD_TEAMWORK_CREATE);
-    menu.addMenuItem(CMD_TEAMWORK_LOCK);
-    menu.addMenuItem(CMD_UPDATE_LOCKS);
-    menu.addMenuDivider();
-    menu.addMenuItem(CMD_TEAMWORK_COMMIT);
-    menu.addMenuItem(CMD_TEAMWORK_UPDATE);
-    menu.addMenuDivider();
-    menu.addMenuItem(CMD_TEAMWORK_CONFIGURE);
+//     var menu;
+//     menu = MenuManager.addMenu(CMD_TEAMWORK);
+//     menu.addMenuItem(CMD_TEAMWORK_OPEN);
+//     menu.addMenuItem(CMD_TEAMWORK_CREATE);
+//     menu.addMenuItem(CMD_TEAMWORK_LOCK);
+//     menu.addMenuItem(CMD_UPDATE_LOCKS);
+//     menu.addMenuDivider();
+//     menu.addMenuItem(CMD_TEAMWORK_COMMIT);
+//     menu.addMenuItem(CMD_TEAMWORK_UPDATE);
+//     menu.addMenuDivider();
+//     menu.addMenuItem(CMD_TEAMWORK_CONFIGURE);
 
-    var contextMenuDiagram;
-    contextMenuDiagram = ContextMenuManager.getContextMenu(DefaultMenus.contextMenus.DIAGRAM);
-    contextMenuDiagram.addMenuItem(CMD_LOCK_ELEMENT);
-    contextMenuDiagram.addMenuItem(CMD_UNLOCK_ELEMENT);
+//     var contextMenuDiagram;
+//     contextMenuDiagram = ContextMenuManager.getContextMenu(DefaultMenus.contextMenus.DIAGRAM);
+//     contextMenuDiagram.addMenuItem(CMD_LOCK_ELEMENT);
+//     contextMenuDiagram.addMenuItem(CMD_UNLOCK_ELEMENT);
 
-    var contextMenuExplorer;
-    contextMenuExplorer = ContextMenuManager.getContextMenu(DefaultMenus.contextMenus.EXPLORER);
-    contextMenuExplorer.addMenuItem(CMD_LOCK_ELEMENT);
-    contextMenuExplorer.addMenuItem(CMD_UNLOCK_ELEMENT);
+//     var contextMenuExplorer;
+//     contextMenuExplorer = ContextMenuManager.getContextMenu(DefaultMenus.contextMenus.EXPLORER);
+//     contextMenuExplorer.addMenuItem(CMD_LOCK_ELEMENT);
+//     contextMenuExplorer.addMenuItem(CMD_UNLOCK_ELEMENT);
 
-    AppInit.appReady(function() {
-        TeamworkView.init();
-    });
+//     AppInit.appReady(function() {
+//         TeamworkView.init();
+//     });
 
-    AppInit.htmlReady(function () {
-        ExtensionUtils.loadStyleSheet(module, "styles/dialog.css");
-        ExtensionUtils.loadStyleSheet(module, "styles/preferences.css");
-        ExtensionUtils.loadStyleSheet(module, "styles/bootstrap/css/bootstrap.css");
-        LockingAttributesElement.addLockingAttributeToElementType();
-        LockingAttributesElement.addLockingAttributesToUMLModelElementType();
-        Trigger.setupTriggerOpenProject();
-        Trigger.setupTriggerOnRepository();
-        Trigger.setupTriggerCommitProject();
-        Trigger.setupTriggerCreateProject();
-        //Trigger.setupTriggerOnDiagramChanges();
-    });
-});
+//     AppInit.htmlReady(function () {
+//         ExtensionUtils.loadStyleSheet(module, "styles/dialog.css");
+//         ExtensionUtils.loadStyleSheet(module, "styles/preferences.css");
+//         ExtensionUtils.loadStyleSheet(module, "styles/bootstrap/css/bootstrap.css");
+//         LockingAttributesElement.addLockingAttributeToElementType();
+//         LockingAttributesElement.addLockingAttributesToUMLModelElementType();
+//         Trigger.setupTriggerOpenProject();
+//         Trigger.setupTriggerOnRepository();
+//         Trigger.setupTriggerCommitProject();
+//         Trigger.setupTriggerCreateProject();
+//         //Trigger.setupTriggerOnDiagramChanges();
+//     });
+// });
+
+function handleShowMessage() {
+    window.alert('Hello, world!')
+}
+
+function init() {
+    app.commands.register('helloworld:show-message', handleShowMessage)
+}
+
+exports.init = init
